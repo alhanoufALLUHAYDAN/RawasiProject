c79bf03a293feb6f9a28ab6e88417139a0edc469
Areej Shareefi <aomsh1420@gmail.com>
2024-12-10 02:18:48 +0300

    start in homepage

--- Rawasi/Rawasi/urls.py
+++ Rawasi/Rawasi/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("main.urls")),
-    path('accounts/',include("accounts.urls")),
-    path('dashboard/', include("dashboard.urls")),
-    path('investment_fund/',include("investment_fund.urls")),
-    path('wallet/', include("wallet.urls")),
+   # path('accounts/',include("accounts.urls")),
+   # path('dashboard/', include("dashboard.urls")),
+   # path('investment_fund/',include("investment_fund.urls")),
+   # path('wallet/', include("wallet.urls")),
 ]  +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Rawasi/main/views.py
+++ Rawasi/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib import messages
+from django.core.paginator import Paginator
 # Create your views here.
+
+def home_view(request:HttpRequest):
+    return render(request,'main/home.html')
