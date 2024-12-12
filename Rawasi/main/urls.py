950f084aace32ad940e60c1b3af85d240dc018a5
Areej Shareefi <aomsh1420@gmail.com>
2024-12-12 12:43:57 +0300

    second merge

--- Rawasi/main/urls.py
+++ Rawasi/main/urls.py
@@ -6,4 +6,6 @@ app_name="main"
 urlpatterns=[
     path('',views.home_view,name="home_view"),
     path('about/',views.about_view,name="about_view"),
+    path('dashboard/leader/',views.leader_dashboard_view,name="leader_dashboard_view"),
+
 ]
